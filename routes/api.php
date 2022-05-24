9818bbd56a82127c4ab1e50ea139995459a513de
kyawthiha <kyawthiha>
2022-05-25 01:25:05 +0630

    feat:cors config and api/me

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/auth/me', function (Request $request) {
     return $request->user();
 });
